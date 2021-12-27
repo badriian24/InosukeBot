b94cc9294d5d6f444352787cb8ecc925c6b02bd5
Bdrxzzzz <75257533+badriian24@users.noreply.github.com>
2021-12-27 07:33:25 +0700

    Add files via upload

---
+++ src/Client/Configs/config.js
@@ -0,0 +1,14 @@
+const client = require("../../index");
+
+module.exports = {
+  token: "TOKENBOT",
+  prefix: "i!",
+  embedcolor: "#2f3136",
+	footer: "Hashibira Inosuke",
+	mongooseConnectionString: "mongodb://localhost:28015",
+  owners: [
+		"776714971782053889"
+	],
+	everyoneMention: false,
+	hostedBy: true
+}
---
+++ src/Client/Prefix/index.js
@@ -0,0 +1,2 @@
+const client = require("../../index");
+
